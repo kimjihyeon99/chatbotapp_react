59efd39e85abbde8aa8e42d8fa3981cd949cfc02
kimjihyeon99 <44187194+kimjihyeon99@users.noreply.github.com>
2021-12-08 11:55:59 +0900

    make textQueryFunction

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,15 +1,54 @@
+import Axios from 'axios';
 import React from 'react';
 
 function Chatbot() {
 
+    const textQuery = async (text)=>{
+        //first need to take care of the message i sent
+        let conversation={
+            who:'user',
+            content:{
+                text:{
+                    text: text
+                }
+            }
+        }
+
+        const textQueryVariables={
+            text
+        }
+         //second need to take care of the message chatbot sent
+        try{
+            //send request to the textQuery
+            const response = await Axios.post('/api/dialogflow/textQuery', textQueryVariables)
+            const content = response.data.fulfillmentMessages[0]
+
+            conversation={
+                who:'bot',
+                content:content
+            }
+            console.log(conversation)
+        }catch(error){
+            conversation={
+                who:'bot',
+                content:{
+                    text:{
+                        text: "Error just occured, please check the problem"
+                    }
+                }
+            }
+        }
+
+    }
+
     const keyPressHandler =(e)=>{
         if(e.key === "Enter"){
 
-            if(e.target.value){
+            if(!e.target.value){
                 return alert('you need to type something first')
             }
             //we will send reqest to text query route
-
+            textQuery(e.target.value)
             // textQuery(e.target.value)
             e.target.value ="";
 
